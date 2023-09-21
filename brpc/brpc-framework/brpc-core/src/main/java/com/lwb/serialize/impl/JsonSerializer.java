2d6223cc64c1a5cf4634181708e864474f3ca120
weibo liu <1974226194@qq.com>
2023-09-21 10:25:18 +0800

    add JsonSerializer(无法序列化String.class)

--- brpc/brpc-framework/brpc-core/src/main/java/com/lwb/serialize/impl/JsonSerializer.java
+++ brpc/brpc-framework/brpc-core/src/main/java/com/lwb/serialize/impl/JsonSerializer.java
@@ -1,15 +1,36 @@
 package com.lwb.serialize.impl;
 
+import com.alibaba.fastjson2.JSON;
 import com.lwb.serialize.Serializer;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class JsonSerializer implements Serializer {
     @Override
     public byte[] serialize(Object object) {
-        return new byte[0];
+        if (object == null) {
+            return null;
+        }
+        byte[] result = JSON.toJSONBytes(object);
+        if (log.isDebugEnabled()) {
+            log.debug("对象【{}】已经完成了序列化操作，序列化后的字节数为【{}】",object, result.length);
+        }
+        return result;
     }
 
     @Override
     public <T> T deserialize(byte[] bytes, Class<T> clazz) {
-        return null;
+        if (bytes == null || clazz == null) {
+            return null;
+        }
+        T t = JSON.parseObject(bytes, clazz);
+        if (log.isDebugEnabled()) {
+            log.debug("类【{}】已经完成了反序列化操作。", clazz);
+        }
+        return t;
+    }
+
+    public static void main(String[] args) {
+
     }
 }
